eef80373ddcd86a7df9d8050a995b9eaf0437827
Shayan Akhtar <149806799+shayan-codes-405@users.noreply.github.com>
2026-07-05 23:28:09 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.4%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -11,7 +11,3 @@ class Solution {
         return res;
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
