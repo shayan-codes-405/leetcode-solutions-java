d7f2e03545a5e0421f83647e501d285ac11275b5
Shayan Akhtar <149806799+shayan-codes-405@users.noreply.github.com>
2026-07-06 12:15:56 +0530

    Time: 1 ms (29.71%), Space: 47 MB (91.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,35 @@
 class Solution {
     public int missingNumber(int[] nums) {
-       //Using Sum Approach
-       int n = nums.length;
-       int sum = 0;
-       for(int i = 0;i<n;i++){
-        sum = sum + nums[i];
-       } 
-       int expSum = n*(n+1)/2;
-      int  miss = expSum - sum;
-       return miss;
+        int n = nums.length;
+        int i = 0;
+        while(i<n){
+            int correct = nums[i]; // In the range [0, n], each value belongs at the index equal to its value.
+            if(nums[i] < n && nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+        for(int index = 0; index < n;index++){
+            if(index!=nums[index]){
+                return index;
+            }
+        }
+        //case 2
+        return n;
+    }
+    void swap(int[] arr,int i,int correct){
+        int temp = arr[i];
+        arr[i] = arr[correct];
+        arr[correct] = temp;
     }
 }
+       //Using Sum Approach
+    //    int n = nums.length;
+    //    int sum = 0;
+    //    for(int i = 0;i<n;i++){
+    //     sum = sum + nums[i];
+    //    } 
+    //    int expSum = n*(n+1)/2;
+    //   int  miss = expSum - sum;
+    //    return miss;
