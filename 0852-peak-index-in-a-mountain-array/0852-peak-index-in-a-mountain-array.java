633e89c96f1e71e799435d2f81a5da3e40b24da4
Shayan Akhtar <149806799+shayan-codes-405@users.noreply.github.com>
2026-07-07 11:44:31 +0530

    Time: 0 ms (100%), Space: 80.4 MB (22.07%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,25 +1,27 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int start = 0;
-        int end = arr.length - 1;
-
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (arr[mid] > arr[mid+1]) {
-                // you are in dec part of array
-                // this may be the ans, but look at left
-                // this is why end != mid - 1
-                end = mid;
-            } else {
-                // you are in asc part of array
-                start = mid + 1; // because we know that mid+1 element > mid element
+        int n = arr.length;
+        int s = 0;
+        int e = n -1;
+        int ans = -1;
+        while(s<=e){
+            int mid = s + (e-s) / 2;
+            if(arr[mid]<arr[mid+1]){
+                //main ascending order part main hu
+                //iska matlab mujhe main left part main hu 
+                //or mujhe pata h answer right part main hai
+                //to fir main right move karunga 
+                s = mid + 1;
+            }
+            else{
+                //arr[mid] >= arr[mid+1]
+                //iska matlab main right part main hu;
+                //iska matalab ek pootentaol answer milgaya 
+                ans = mid;
+                //now i have to find the 
+                e = mid - 1;
             }
         }
-        // in the end, start == end and pointing to the largest number because of the 2 checks above
-        // start and end are always trying to find max element in the above 2 checks
-        // hence, when they are pointing to just one element, that is the max one because that is what the checks say
-        // more elaboration: at every point of time for start and end, they have the best possible answer till that time
-        // and if we are saying that only one item is remaining, hence cuz of above line that is the best possible ans
-        return start; // or return end as both are =
+        return ans;
     }
 }
